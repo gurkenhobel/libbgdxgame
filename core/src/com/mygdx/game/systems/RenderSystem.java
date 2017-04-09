83c0161129ae9156434c4ecd973025ba651afd08
gurkenhobel <nilsw.agner@web.de>
2017-04-09 15:30:32 +0200

    added clipping in rendersystem

--- core/src/com/mygdx/game/systems/RenderSystem.java
+++ core/src/com/mygdx/game/systems/RenderSystem.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.utils.ScissorStack;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -25,6 +27,7 @@ public class RenderSystem extends SortedIteratingSystem {
     private static ComponentMapper<RenderComponent> _rm;
     private static ComponentMapper<PositionComponent> _pm;
     private static ComponentMapper<SizeComponent> _sm;
+
 
 
 
@@ -56,11 +59,14 @@ public class RenderSystem extends SortedIteratingSystem {
         Gdx.gl.glClearColor(0.42f, 0.43f, 0.45f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+
+
         _batch.begin();
+        generateClippingScissors();
         _batch.setProjectionMatrix(_camera.combined);
         super.update(deltaTime);
         _batch.end();
-
+        ScissorStack.popScissors();
     }
 
     @Override
@@ -75,6 +81,14 @@ public class RenderSystem extends SortedIteratingSystem {
     public void resize(int width, int height) {
         _viewport.update(width, height, true);
         _camera.position.set(_camera.viewportWidth / 2, _camera.viewportHeight / 2, 0);
+    }
+
+    private void generateClippingScissors(){
+        Rectangle scissors = new Rectangle();
+        Rectangle clipBounds = new Rectangle(_viewport.getScreenX(),_viewport.getScreenY(),
+                _viewport.getScreenWidth(),_viewport.getScreenHeight());
+        ScissorStack.calculateScissors(_camera, _batch.getTransformMatrix(), clipBounds, scissors);
+        ScissorStack.pushScissors(scissors);
     }
 
     private static class ZComparator implements Comparator<Entity> {
