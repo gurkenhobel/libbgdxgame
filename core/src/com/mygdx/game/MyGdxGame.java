5e59f9e348de53a87c3160ba6070780123ba7a38
gurkenhobel <nilsw.agner@web.de>
2017-04-15 13:23:41 +0200

    added most basic tilemap implementation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,31 +2,61 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.Logger;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MyGdxGame extends ApplicationAdapter {
-    SpriteBatch _batch;
-    SheetAnimation _anim;
+    private OrthographicCamera _camera;
+
+    private TiledMap _map;
+    private OrthogonalTiledMapRenderer _mapRenderer;
+    private OrthoCamController _camController;
 
     @Override
     public void create() {
-        _batch = new SpriteBatch();
-        _anim = new SheetAnimation("spritesheet.png", 4, 4, 25, 50, 50);
+        try {
+            float w = Gdx.graphics.getWidth();
+            float h = Gdx.graphics.getHeight();
+
+
+            _camera = new OrthographicCamera();
+            _camera.setToOrtho(false,(w/h) * 6, 6 );
+            _camera.update();
+
+            _camController = new OrthoCamController(_camera);
+            Gdx.input.setInputProcessor(_camController);
+
+            _map = new TmxMapLoader().load("test-map.tmx");
+            _mapRenderer = new OrthogonalTiledMapRenderer(_map, 1/64f);
+        }
+        catch(Exception ex){
+            Gdx.app.log("init", ex.getMessage(), ex);
+        }
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0.42f, 0.43f, 0.45f, 1);
+        Gdx.gl.glClearColor(0.55f, 0.55f, 0.55f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        _batch.begin();
-        _anim.sampleAndDraw(_batch);
-        _batch.end();
+        _camera.update();
+
+
+        _mapRenderer.setView((OrthographicCamera) _camera);
+        _mapRenderer.render();
     }
 
     @Override
     public void dispose() {
-        _batch.dispose();
-        _anim.dispose();
+        _mapRenderer.dispose();
+        _map.dispose();
     }
 }
